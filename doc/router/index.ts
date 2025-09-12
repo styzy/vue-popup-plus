bf4af49f4204bc01b7272bf21ac39f358247151d
styzy <styzyreal@126.com>
2025-09-12 19:19:13 +0800

    feat: 切换文档路由为hash模式

--- doc/router/index.ts
+++ doc/router/index.ts
@@ -1,8 +1,7 @@
-import { defineAsyncComponent } from 'vue'
-import { createRouter, createWebHistory, type Router } from 'vue-router'
+import { createRouter, createWebHashHistory, type Router } from 'vue-router'
 
 const router: Router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
+	history: createWebHashHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
 			path: '/',
