1a17cb2df3a2742606c4dd439a3cf75ae6e1c18b
styzy <styzyreal@126.com>
2025-10-28 01:12:01 +0800

    feat: 核心升级v1.1.4

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -9,11 +9,11 @@ import { POPUP_ANIMATIONS } from 'vue-popup-plus'
 export default defineComponent({
 	name: 'App',
 	created() {
-		// this.$popup.render({
-		// 	component: () => import('@/views/Demo.vue'),
-		// 	maskAnimation: POPUP_ANIMATIONS.CUSTOM,
-		// })
-		// this.$popup.test('hello world')
+		this.$popup.render({
+			component: () => import('@/views/Demo.vue'),
+			maskAnimation: POPUP_ANIMATIONS.CUSTOM,
+		})
+		this.$popup.toast('hello world')
 	},
 })
 </script>
