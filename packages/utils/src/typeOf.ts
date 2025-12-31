97341852ca94a1043bde74f32f15726eb2466ffd
styzy <styzyreal@126.com>
2026-01-01 04:08:08 +0800

    feat: utils 工具库 typeOf方法优化

--- packages/utils/src/typeOf.ts
+++ packages/utils/src/typeOf.ts
@@ -1,16 +1,17 @@
-type TypeOf =
-	| 'Null'
-	| 'Undefined'
-	| 'Object'
-	| 'Array'
-	| 'String'
-	| 'Number'
-	| 'Boolean'
-	| 'Function'
-	| 'Symbol'
-	| 'BigInt'
+export const enum TypeEnum {
+	Null = 'Null',
+	Undefined = 'Undefined',
+	Object = 'Object',
+	Array = 'Array',
+	String = 'String',
+	Number = 'Number',
+	Boolean = 'Boolean',
+	Function = 'Function',
+	Symbol = 'Symbol',
+	BigInt = 'BigInt',
+}
 
-export function typeOf(param: any): TypeOf {
+export function typeOf(param: any): TypeEnum {
 	const result = Object.prototype.toString.call(param).match(/\s+(\w+)/)
-	return result ? (result[1] as TypeOf) : 'Null'
+	return result ? (result[1] as TypeEnum) : TypeEnum.Null
 }
