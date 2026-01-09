4d83be8b847079991bda597459eccb18d315113d
styzy <styzyreal@126.com>
2026-01-09 19:46:11 +0800

    feat(plugin): 新增createPresetPlugin()函数用于创建预置插件，同时将plugin导出的预置插件废弃，计划在1.7.0版本移除

--- packages/plugin/src/index.ts
+++ packages/plugin/src/index.ts
@@ -1,4 +1,11 @@
-import { definePlugin, version as coreVersion } from 'vue-popup-plus'
+import {
+	version as coreVersion,
+	definePlugin,
+	LogGroupItemType,
+	LogType,
+	printLog,
+} from 'vue-popup-plus'
+import { PluginLog } from './log'
 import { album, type IAlbum } from './plugins/album'
 import { alert, type IAlert } from './plugins/alert'
 import { confirm, type IConfirm } from './plugins/confirm'
@@ -10,11 +17,15 @@ import type { GlobalPluginOption } from './typings'
 
 import './assets/styles/main.scss'
 
+export { type Skin } from './skin'
 export { version } from './version'
 export type { IAlbum, IAlert, IConfirm, IDialog, ILoading, IPrompt, IToast }
 
-export { album, alert, confirm, dialog, loading, prompt, toast }
-
+/**
+ * @deprecated 从 1.6.0 开始，推荐使用 {@link createPresetPlugin} 来创建预置插件
+ *
+ * - 将在 1.7.0 移除
+ */
 export const plugin = definePlugin({
 	name: 'plugin-preset',
 	author: 'STYZY',
@@ -34,3 +45,62 @@ export const plugin = definePlugin({
 		toast.install(config, options)
 	},
 })
+
+export type PresetPluginOption = GlobalPluginOption & {}
+
+/**
+ * 创建预置插件
+ *
+ * - 可全局定义默认皮肤
+ * - 可全局配置所有子插件的默认参数
+ */
+export function createPresetPlugin(options?: PresetPluginOption) {
+	const { skin = 'modern' } = options || {}
+
+	return definePlugin({
+		name: 'plugin-preset',
+		author: 'STYZY',
+		requiredCoreVersion: {
+			min: coreVersion,
+			max: coreVersion,
+		},
+		install(config) {
+			album.install(config, { skin })
+			alert.install(config, { skin })
+			confirm.install(config, { skin })
+			dialog.install(config, { skin })
+			loading.install(config, { skin })
+			prompt.install(config, { skin })
+			toast.install(config, { skin })
+
+			printLog(
+				new PluginLog({
+					type: LogType.Success,
+					caller: {
+						name: 'createPlugin()',
+						type: 'Function',
+						value: createPresetPlugin,
+					},
+					message: '创建预置插件 plugin-preset 成功',
+					group: [
+						{
+							type: LogGroupItemType.Info,
+							title: '皮肤',
+							content:
+								skin === 'modern'
+									? 'modern 现代'
+									: 'classic 经典',
+						},
+						{
+							type: LogGroupItemType.Data,
+							title: '调用参数',
+							dataName: 'options',
+							dataType: 'PresetPluginOption',
+							dataValue: options,
+						},
+					],
+				})
+			)
+		},
+	})
+}
