babd615d746834d504763ae055bab600bdfff24b
styzy <styzyreal@126.com>
2026-06-03 01:21:20 +0800

    chroe(plugin): 优化构建

--- packages/plugin/vite.config.umd.ts
+++ packages/plugin/vite.config.umd.ts
@@ -10,18 +10,13 @@ export default defineConfig({
 		lib: {
 			entry: fileURLToPath(new URL('./src/index', import.meta.url)),
 			name: 'VuePopupPlusPluginPreset',
-			formats: ['es'],
+			formats: ['es', 'umd'],
 		},
-		outDir: fileURLToPath(new URL('./es', import.meta.url)),
+		outDir: fileURLToPath(new URL('./dist', import.meta.url)),
 		rollupOptions: {
 			// 确保外部化处理那些你不想打包进库的依赖
 			external: ['vue', 'vue-popup-plus'],
 			output: {
-				// 每个源文件单独输出、不合并chunk
-				preserveModules: true,
-				// 剔除dist/es下多余src目录
-				preserveModulesRoot: 'src',
-				entryFileNames: '[name].mjs',
 				// 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
 				globals: {
 					vue: 'Vue',
@@ -43,10 +38,10 @@ export default defineConfig({
 			tsconfigPath: fileURLToPath(
 				new URL('./tsconfig.types.json', import.meta.url)
 			),
-			rollupTypes: false,
-			outDir: fileURLToPath(new URL('./es', import.meta.url)),
+			rollupTypes: true,
+			outDir: fileURLToPath(new URL('./dist', import.meta.url)),
 			pathsToAliases: true,
-			// insertTypesEntry: true,
+			insertTypesEntry: true,
 		}),
 	],
 })
