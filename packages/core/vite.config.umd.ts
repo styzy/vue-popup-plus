a66469ccb4b86d0c0067c228a862e2b2e8e3827e
styzy <styzyreal@126.com>
2026-06-03 11:58:49 +0800

    chroe(core): 优化构建，独立构建es和umd格式

--- packages/core/vite.config.umd.ts
+++ packages/core/vite.config.umd.ts
@@ -1,7 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
-import dts from 'vite-plugin-dts'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import vue from '@vitejs/plugin-vue'
 
@@ -9,21 +8,19 @@ export default defineConfig({
 	build: {
 		lib: {
 			entry: fileURLToPath(new URL('./src/index', import.meta.url)),
-			formats: ['es'],
+			name: 'VuePopupPlus',
+			formats: ['umd'],
 		},
-		outDir: fileURLToPath(new URL('./es', import.meta.url)),
+		outDir: fileURLToPath(new URL('./dist', import.meta.url)),
 		rollupOptions: {
 			// 确保外部化处理那些你不想打包进库的依赖
-			external: ['vue'],
+			external: ['vue', 'vue-popup-plus'],
 			output: {
-				// 每个源文件单独输出、不合并chunk
-				preserveModules: true,
-				// 剔除dist/es下多余src目录
-				preserveModulesRoot: 'src',
-				entryFileNames: '[name].mjs',
+				entryFileNames: 'index.umd.js',
 				// 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
 				globals: {
 					vue: 'Vue',
+					'vue-popup-plus': 'VuePopupPlus',
 				},
 			},
 		},
@@ -33,18 +30,5 @@ export default defineConfig({
 			'@core': fileURLToPath(new URL('./src', import.meta.url)),
 		},
 	},
-	plugins: [
-		vue(),
-		vueDevTools(),
-		cssInjectedByJsPlugin(),
-		dts({
-			tsconfigPath: fileURLToPath(
-				new URL('./tsconfig.types.json', import.meta.url)
-			),
-			rollupTypes: false,
-			outDir: fileURLToPath(new URL('./es', import.meta.url)),
-			entryRoot: fileURLToPath(new URL('./src', import.meta.url)),
-			pathsToAliases: true,
-		}),
-	],
+	plugins: [vue(), vueDevTools(), cssInjectedByJsPlugin()],
 })
