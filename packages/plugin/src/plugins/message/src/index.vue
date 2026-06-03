902d5e12dd819361030b850da40dbf2eed842835
吴杰 <wujie@carzsoft.com>
2026-06-03 16:48:42 +0800

    feat(plugin): 优化 Message 消息组件

--- packages/plugin/src/plugins/message/src/index.vue
+++ packages/plugin/src/plugins/message/src/index.vue
@@ -1,25 +1,32 @@
 <template>
 	<div :class="ns.block()">
 		<TransitionGroup
-			name="popup-message"
+			:name="ns.block()"
 			tag="div"
-			:class="ns.element('inner')">
-			<PMessage
-				v-for="item in messages"
-				:key="item.id"
-				:id="item.id"
-				:skin="skin"
-				:content="item.content"
-				:theme="item.theme"
-				:duration="item.duration"
-				:showClose="item.showClose"
-				:hoverWait="item.hoverWait"
-				:onClose="() => emitClose(item.id)" />
+			:class="[
+				ns.element('inner'),
+				ns.elementModifier('inner', placement),
+			]">
+			<template v-if="isMounted">
+				<PMessage
+					v-for="item in messages"
+					:key="item.id"
+					:id="item.id"
+					:skin="skin"
+					:content="item.content"
+					:theme="item.theme"
+					:duration="item.duration"
+					:showClose="item.showClose"
+					:hoverWait="item.hoverWait"
+					:onClose="() => emitClose(item.id)"
+			/></template>
 		</TransitionGroup>
 	</div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { type PopupPlacement } from 'vue-popup-plus'
 import { POPUP_INSIDE_COMPONENT_NAMES } from '@plugin/CONSTANTS'
 import { useNamespace } from '@plugin/hooks'
 import type { PopupSkin } from '@plugin/skin'
@@ -36,9 +43,17 @@ const ns = useNamespace(POPUP_INSIDE_COMPONENT_NAMES.MESSAGE_GROUP)
 type Props = {
 	skin: PopupSkin
 	messages: PopupMessageRecord[]
+	placement: PopupPlacement
+	animationDuration: number
 }
 
-const { skin, messages } = defineProps<Props>()
+const { skin, messages, placement, animationDuration } = defineProps<Props>()
+
+const isMounted = ref(false)
+
+onMounted(() => {
+	isMounted.value = true
+})
 
 type Emits = {
 	messageClose: [id: string]
@@ -53,35 +68,121 @@ const emitClose = (id: string) => emit('messageClose', id)
 @use '@plugin/assets/styles/inject.scss' as *;
 
 @include ns-block('message-group') {
-	padding: 20px;
 	z-index: inherit;
 	pointer-events: none;
+	width: 100%;
+	height: 100%;
 
 	@include ns-element('inner') {
+		padding: 20px;
 		display: flex;
-		align-items: center;
-		flex-direction: column;
 		gap: use-spacing(small);
-		width: 30vw;
-	}
+		.popup-message-group-enter-active,
+		.popup-message-group-leave-active {
+			transition: v-bind('`all ${animationDuration}ms ease`');
+		}
+		.popup-message-group-move {
+			transition: v-bind('`all ${animationDuration}ms ease`');
+		}
+		.popup-message-group-enter-from {
+			opacity: 0;
+		}
 
-	.popup-message-enter-active,
-	.popup-message-leave-active {
-		transition: all 0.25s ease;
-	}
+		.popup-message-group-leave-to {
+			opacity: 0;
+		}
+		@include ns-modifier('left-top') {
+			align-items: flex-start;
+			flex-direction: column;
+			.popup-message-group-enter-from {
+				transform: translateX(-20px);
+			}
 
-	.popup-message-enter-from {
-		opacity: 0;
-		transform: translateY(-12px);
-	}
+			.popup-message-group-leave-to {
+				transform: translateX(-20px);
+			}
+		}
+		@include ns-modifier('left') {
+			align-items: flex-start;
+			flex-direction: column;
+			.popup-message-group-enter-from {
+				transform: translateX(-20px);
+			}
 
-	.popup-message-leave-to {
-		opacity: 0;
-		transform: translateY(-12px);
-	}
+			.popup-message-group-leave-to {
+				transform: translateX(-20px);
+			}
+		}
+		@include ns-modifier('left-bottom') {
+			align-items: flex-start;
+			flex-direction: column-reverse;
+			.popup-message-group-enter-from {
+				transform: translateX(-20px);
+			}
 
-	.popup-message-move {
-		transition: transform 0.25s ease;
+			.popup-message-group-leave-to {
+				transform: translateX(-20px);
+			}
+		}
+		@include ns-modifier('top') {
+			align-items: center;
+			flex-direction: column;
+			.popup-message-group-enter-from {
+				transform: translateY(-20px);
+			}
+
+			.popup-message-group-leave-to {
+				transform: translateY(-20px);
+			}
+		}
+		@include ns-modifier('center') {
+			align-items: center;
+			flex-direction: column;
+		}
+		@include ns-modifier('bottom') {
+			align-items: center;
+			flex-direction: column-reverse;
+			.popup-message-group-enter-from {
+				transform: translateY(20px);
+			}
+
+			.popup-message-group-leave-to {
+				transform: translateY(20px);
+			}
+		}
+		@include ns-modifier('right-top') {
+			align-items: flex-end;
+			flex-direction: column;
+			.popup-message-group-enter-from {
+				transform: translateX(20px);
+			}
+
+			.popup-message-group-leave-to {
+				transform: translateX(20px);
+			}
+		}
+		@include ns-modifier('right') {
+			align-items: flex-end;
+			flex-direction: column;
+			.popup-message-group-enter-from {
+				transform: translateX(20px);
+			}
+
+			.popup-message-group-leave-to {
+				transform: translateX(20px);
+			}
+		}
+		@include ns-modifier('right-bottom') {
+			align-items: flex-end;
+			flex-direction: column-reverse;
+			.popup-message-group-enter-from {
+				transform: translateX(20px);
+			}
+
+			.popup-message-group-leave-to {
+				transform: translateX(20px);
+			}
+		}
 	}
 }
 </style>
