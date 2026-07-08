c48884a4a6bb2cf697be24631a0889b7c0cce160
styzy <styzyreal@126.com>
2026-07-08 19:26:31 +0800

    chroe(core): 移除冗余构建配置

--- packages/core/vite.config.umd.ts
+++ packages/core/vite.config.umd.ts
@@ -14,13 +14,12 @@ export default defineConfig({
 		outDir: fileURLToPath(new URL('./dist', import.meta.url)),
 		rollupOptions: {
 			// 确保外部化处理那些你不想打包进库的依赖
-			external: ['vue', 'vue-popup-plus'],
+			external: ['vue'],
 			output: {
 				entryFileNames: 'index.umd.js',
 				// 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
 				globals: {
 					vue: 'Vue',
-					'vue-popup-plus': 'VuePopupPlus',
 				},
 			},
 		},
